11f092302df2a574e7329a2a5e88ece5a8ba7f92
Grechanin <grechanin@gmail.com>
2019-07-26 22:19:38 +0300

    Updates

---
+++ precache-manifest.b4af326eb12ac7cac1d4fda291550d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cfa0e0188a715bdef369141fa1038e",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "06d29490c2aff305bb00",
+    "url": "/react-test-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f041749acd5ca4852c09",
+    "url": "/react-test-app/static/css/main.8f8c9aac.chunk.css"
+  },
+  {
+    "revision": "06d29490c2aff305bb00",
+    "url": "/react-test-app/static/js/2.71b76905.chunk.js"
+  },
+  {
+    "revision": "f041749acd5ca4852c09",
+    "url": "/react-test-app/static/js/main.2a7d7bb4.chunk.js"
+  },
+  {
+    "revision": "41d7d8264a454f21780b",
+    "url": "/react-test-app/static/js/runtime~main.cbd17629.js"
+  }
+]);
